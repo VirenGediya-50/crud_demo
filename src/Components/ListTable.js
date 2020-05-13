85d620dfc7388a7f6f52f0d1d6349594d6e00779
VIRENGEDIYA <virengediya50@gmail.com>
2020-05-13 19:05:12 +0530

    Add changes

--- src/Components/ListTable.js
+++ src/Components/ListTable.js
@@ -1,48 +1,50 @@
 import React from 'react';
-import { Table } from 'react-bootstrap';
+import { Table, ListGroup } from 'react-bootstrap';
 import CommonButton from './CommonButton';
  
 const ListTable = (props) => {
     const { data, onEditClick, onDeleteClick } = props;
     return (
-        <Table responsive>
-            <thead>
-                <tr>
-                    <th className={"header_id"}>Id</th>
-                    <th>Name</th>
-                    <th className={"header_action"}>Action</th>
-                </tr>
-            </thead>
-            <tbody>
-            { data.length > 0 ?
-                data.map((user, id) =>(
-                    <tr key={id}>
-                        <td>{(id+1)}</td>
-                        <td>{user.firstName+" "+ user.lastName}</td>
-                        <td className={"action_layout"}>
-                            <CommonButton
-                                title={"Edit"}
-                                variant={'primary'}
-                                className={"delete_button"}
-                                onClick={() => onEditClick(id)}
-                            />
-                            <CommonButton 
-                                title={"Delete"}
-                                variant={'danger'}
-                                className={"delete_button"}
-                                onClick={() => onDeleteClick(id)}
-                            />
-                        </td>
+        <div className="table_scroll">
+            <Table responsive >
+                <thead>
+                    <tr>
+                        <th className={"header_id"}>Id</th>
+                        <th>Name</th>
+                        <th className={"header_action"}>Action</th>
                     </tr>
-                ))
-                :   
-                <tr>
-                    <td colSpan="3"><i>No data available</i></td>
-                </tr>
-                // <h6>No user Data available</h6>
-            }
-            </tbody>
-        </Table>
+                </thead>
+                <tbody>
+                { data.length > 0 ?
+                    data.map((user, id) =>(
+                        <tr key={id}>
+                            <td>{(id+1)}</td>
+                            <td>{user.firstName+" "+ user.lastName}</td>
+                            <td className={"action_layout"}>
+                                <CommonButton
+                                    title={"Edit"}
+                                    variant={'primary'}
+                                    className={"delete_button"}
+                                    onClick={() => onEditClick(id)}
+                                />
+                                <CommonButton 
+                                    title={"Delete"}
+                                    variant={'danger'}
+                                    className={"delete_button"}
+                                    onClick={() => onDeleteClick(id)}
+                                />
+                            </td>
+                        </tr>
+                    ))
+                    :   
+                    <tr>
+                        <td colSpan="3"><i>No data available</i></td>
+                    </tr>
+                    // <h6>No user Data available</h6>
+                }
+                </tbody>
+            </Table>
+        </div>
     );
 }
  
